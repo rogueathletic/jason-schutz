3bdb337479c2755baed73b5f1be6d6b575ec2724
Jason Schutz <jason@rogueathletic.com>
2019-02-12 17:52:04 -0800

    better balanced link to code copy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,2 @@
-/* <button onclick="scrollWin(100, 0)">Scroll right</button><br><br>
-
-<button onclick="scrollWin(-100, 0)">Scroll left</button><br><br>
-
-let scrollRight = ("#right-button")
-
-
-var scrollRight = function(){
-    scrollWin(100, 0) {
-        window.scrollBy(x, y);
-      }
-}
-
-
-function myFunction() {
-  document.getElementById("right-button").scroll(100, 0)
-} }*/
-
 
 
